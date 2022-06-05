b8d2bd1c00db079ef49a17f41f950fba6051aa41
rachanne22 <rachel.sickle@gmail.com>
2022-06-05 02:00:21 -0400

    updated files with comments

--- complete/src/main/java/com/example/validatingforminput/PersonForm.java
+++ complete/src/main/java/com/example/validatingforminput/PersonForm.java
@@ -6,12 +6,12 @@ import javax.validation.constraints.Size;
 
 public class PersonForm {
 
-	@NotNull
-	@Size(min=2, max=30)
+	@NotNull //Does not allow a null value, which is what Spring MVC generates if the entry is empty.
+	@Size(min=2, max=30) //Allows names between 2 and 30 characters long.
 	private String name;
 
 	@NotNull
-	@Min(18)
+	@Min(18) //Does not allow the age to be less than 18
 	private Integer age;
 
 	public String getName() {
--- complete/src/main/java/com/example/validatingforminput/WebController.java
+++ complete/src/main/java/com/example/validatingforminput/WebController.java
@@ -12,17 +12,28 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Controller
 public class WebController implements WebMvcConfigurer {
+	//This controller has a GET method and a POST method. Both methods are mapped to /
 
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/results").setViewName("results");
 	}
-
+    //The showForm method returns the form template
 	@GetMapping("/")
 	public String showForm(PersonForm personForm) {
 		return "form";
 	}
 
+	
+	//The checkPersonInfo method accepts two arguments:
+		//1. A personForm object marked with @Valid to gather the attributes filled out in the form
+	    //2. A bindingResult object so that you can test for and retrieve validation errors.
+//	You can retrieve all the attributes from the form, which is bound to the PersonForm object. In the code, you test for errors. If you encounter an error, you can send the user back to the original form template. 
+//	In that situation, all the error attributes are displayed.
+//	If all of the person’s attribute are valid,
+//	it redirects the browser to the final results template.
+	
+	
 	@PostMapping("/")
 	public String checkPersonInfo(@Valid PersonForm personForm, BindingResult bindingResult) {
 
